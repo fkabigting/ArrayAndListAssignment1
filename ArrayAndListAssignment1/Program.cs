449e5489253e950b3e68d81c07b90b126f374e61
fkabigting <leonardfrancis.kabigting1@my-aolcc.com>
2024-01-06 20:51:05 -0700

    Add project files.

---
+++ ArrayAndListAssignment1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayAndList
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            String[] SportList = new string[] { "Basketball", "Volleyball", "Soccer", "Golf", "Bowling" };
+            Console.WriteLine("Please select a number from 1 to 4: ");
+            int YouChose = Convert.ToInt32(Console.ReadLine());
+            
+
+            while (YouChose < 5)
+
+
+            {
+            Console.WriteLine("This number represents: " + SportList[YouChose]);
+            Console.ReadLine();
+            YouChose = 10;
+            }
+
+            Console.Write("You Entered an Invalid Selection");
+            
+
+
+        }
+    }
+}
